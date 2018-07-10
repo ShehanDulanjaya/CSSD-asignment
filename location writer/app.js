71799966db424c79064ce0a35701a03666f7b9c1
ShehanDulanjaya <shehanpraneeth5@gmail.com>
2018-07-10 13:22:56 +0530

    location write to file

---
+++ location writer/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const locationroute= require('./routes/location')
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+app.use('/location',locationroute);
+module.exports = app;
