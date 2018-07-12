6c8aebd247ea87cafe83680f4860ee2f31b66725
ShehanDulanjaya <shehanpraneeth5@gmail.com>
2018-07-12 19:30:42 +0530

    data class readings

--- SmartCity/src/Classes/Data.java
+++ SmartCity/src/Classes/Data.java
@@ -5,10 +5,23 @@
  */
 package Classes;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 /**
  *
  * @author Akila Jayasinghe
  */
 public class Data {
+       private Double readings;
+private static DecimalFormat df2 = new DecimalFormat("0.##");
+   
+    public Data() {
+    }
     
+     public String getReadings() {
+         
+        this.readings = new Random().nextDouble();		
+        return df2.format(readings);
+    }
 }
