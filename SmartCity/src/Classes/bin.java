14f423f784c5a1ba8a954e4d3c49c1580750f326
CJM1995 <40541830+CJM1995@users.noreply.github.com>
2018-07-18 02:29:34 +0530

    Bin function

---
+++ SmartCity/src/Classes/bin.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import File.serialize;
+import java.util.ArrayList;
+/**
+ *
+ * @author CJ
+ */
+public class bin {
+    private bin Bin;
+    ArrayList<Sensor>sens;
+    Location location;
+    private static final bin instance = new bin ();
+    public static bin getinstance(){
+       
+     
+        return instance;
+    }
+    
+    public ArrayList<Sensor> getBins(){
+        this.sens=new ArrayList<>();
+    
+         for(Sensor sensor:serialize.getAllSensors()){
+            if("Bin Sensor".equals(sensor.getSensorType())){
+                this.sens.add(sensor);
+            }  
+    }
+         return this.sens;
+    }
+    public String getlocation(){
+        this.location=new Location();
+        return this.location.getemergency();
+    }
+}
