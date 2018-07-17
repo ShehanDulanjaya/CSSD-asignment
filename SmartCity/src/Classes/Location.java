c789fc456f17e49767824487607448ff517d8fbe
ShehanDulanjaya <shehanpraneeth5@gmail.com>
2018-07-17 19:33:52 +0530

    location observed

--- SmartCity/src/Classes/Location.java
+++ SmartCity/src/Classes/Location.java
@@ -13,6 +13,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.SQLException;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
+import javafx.beans.InvalidationListener;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -22,15 +27,18 @@ import org.json.simple.parser.ParseException;
  *
  * @author Dos
  */
-public class Location {
+public class Location extends Observable{
 
     private double latitude;
     private double longitude;
     private String address;
+    emergencyService observer;
+    private String random;
 
     public Location(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
+        
     }
 
     public double getLatitude() {
@@ -85,4 +93,29 @@ public class Location {
                               }
         return this.address;
     }
+    
+    public String setemergency(){
+        String[] cordinates = {"6.864124613608374/80.0048899434737","6.930399406839585/79.9771101098645","6.941487794409919/79.9135422706604"
+                ,"6.932092591524386/79.88621896274822","6.876004993503434/79.90870034814657","6.930080472159258/79.84503842599281","6.852634708845186/79.9179616201177","6.828754293439086/80.01523604897454"};
+        this.random = (cordinates[new Random().nextInt(cordinates.length)]);
+        notifyObservers(random);
+        return this.random;
+    }
+    
+    public void registerObserver(emergencyService listener) {
+        this.observer=listener;
+         
+    }
+
+    public void unregisterObserver(emergencyService listener) {
+        this.observer=null;
+      
+    }
+
+    public void notifyObservers(String ob) {
+            observer.update(this,ob );
+            
+        }
+               
+    
 }
---
+++ SmartCity/src/Classes/emergencyService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.io.FileWriter;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ *
+ * @author SHEHA
+ */
+public class emergencyService implements Observer{
+
+    public emergencyService() {
+    }
+    
+    public void warnEmergancy(String location){
+        try{    
+           FileWriter fw=new FileWriter("D:\\Github\\CSSD-asignment\\SmartCity\\emergancy.txt");    
+           fw.write(location);    
+           fw.close();    
+          }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }  
+        
+          System.out.println("emergancy warn...");    
+     
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        warnEmergancy((String) arg);
+    }
+    
+}
